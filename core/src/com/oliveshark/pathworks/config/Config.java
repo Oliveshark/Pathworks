f1dd7cf05c5e9037d212726f6a55bbb7a431d443
Fluctus <fluctus137@gmail.com>
2019-10-22 14:19:31 +0200

    Load config from file

--- core/src/com/oliveshark/pathworks/config/Config.java
+++ core/src/com/oliveshark/pathworks/config/Config.java
@@ -1,9 +1,50 @@
 package com.oliveshark.pathworks.config;
 
+import com.badlogic.gdx.utils.JsonReader;
+import com.badlogic.gdx.utils.JsonValue;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+
 public class Config {
+    private static final String CONFIG_FILE = "core/src/com/oliveshark/pathworks/config/config.json";
 
-    public static final int GRID_WIDTH = 32;
-    public static final int GRID_HEIGHT = 24;
+    private static int GRID_WIDTH = 32;
+    private static int GRID_HEIGHT = 24;
+    private static int TILE_DIMENSION = 32;
 
-    public static final int TILE_DIMENSION = 32;
+    public static int getTileDimension() {
+        return TILE_DIMENSION;
+    }
+
+    public static int getGridHeight() {
+        return GRID_HEIGHT;
+    }
+
+    public static int getGridWidth() {
+        return GRID_WIDTH;
+    }
+
+    public static void load(){
+        JsonReader reader = new JsonReader();
+        JsonValue config = null;
+
+        try {
+            config = reader.parse(new FileInputStream(CONFIG_FILE));
+            GRID_WIDTH = config.get("GRID_WIDTH") != null ? config.get("GRID_WIDTH").asInt() : 32;
+            GRID_HEIGHT= config.get("GRID_HEIGHT") != null ? config.get("GRID_HEIGHT").asInt() : 24;
+            TILE_DIMENSION= config.get("TILE_DIMENSION") != null ? config.get("TILE_DIMENSION").asInt() : 32;
+            System.out.println(GRID_WIDTH);
+            System.out.println(GRID_HEIGHT);
+
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
 }
